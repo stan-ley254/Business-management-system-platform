bdfb9713c50c02ad4f55f0a5ae4508371326ef92
Stan <stanley@stanley.justart.com>
2025-10-07 15:14:39 +0300

    start.sh changes

--- docker/start.sh
+++ docker/start.sh
@@ -7,8 +7,8 @@ chown -R www-data:www-data /var/www/html/storage
 chmod -R 775 /var/www/html/storage
 
 # Run artisan commands safely as www-data
-su -s /bin/bash www-data -c "php artisan migrate --force"
-su -s /bin/bash www-data -c "php artisan db:seed --force"
+su -s /bin/bash www-data -c "php artisan migrate:fresh --seed"
+
 su -s /bin/bash www-data -c "php artisan config:clear"
 su -s /bin/bash www-data -c "php artisan config:cache"
 su -s /bin/bash www-data -c "php artisan route:cache"
